096ecb0b59ec8c5938c6a81c773943a065c03ac3
Pnhau <pnhau@cmcglobal.vn>
2023-03-08 13:20:56 +0700

    install ant ui

--- tool-cron-data/src/App.tsx
+++ tool-cron-data/src/App.tsx
@@ -7,6 +7,9 @@ import {
 import CronDataPage from 'pages/CronDataPage';
 import MainPage from 'pages/MainPage';
 
+import 'antd/dist/reset.css';
+import './index.css';
+
 const routers = createBrowserRouter([
   {
     path: "/",
--- tool-cron-data/src/pages/CronDataPage/index.tsx
+++ tool-cron-data/src/pages/CronDataPage/index.tsx
@@ -1,9 +1,14 @@
+import { Button } from "antd";
+
 function CronDataPage() {
   return (
     <div>
       <div>Cron Data Page</div>
       <div>
         <a href={`/main-page`}>Router Link</a>
+      </div>
+      <div>
+        <Button type="primary">Primary Button</Button>
       </div>
     </div>
 
